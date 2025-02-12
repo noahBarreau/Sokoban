ee2012c34dcd1a6a4a6f4db889aef590e52528e2
noahBarreau <nonoferrari04@gmail.com>
2025-02-12 10:25:15 +0100

    tri dans le code

--- Assets/Scripts/GeneralAction.cs
+++ Assets/Scripts/GeneralAction.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class BallAction : MonoBehaviour
+public class GeneralAction : MonoBehaviour
 {
-    public Vector3 respawnPoint = Vector3.zero;
     public PopupManager popupManager;
     public Button startButton;
     public TMP_Text timerText;
     public GameObject chrono;
-    public GameObject spawn;
     public GameObject pausePanel;
 
     public float elapsedTime = 0f;
@@ -22,11 +20,6 @@ public class BallAction : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Respawn();
-        }
-
         if (isTimerRunning)
         {
             elapsedTime += Time.deltaTime;
@@ -39,21 +32,6 @@ public class BallAction : MonoBehaviour
             Time.timeScale = 0f;
         }
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Trap"))
-        {
-            Debug.Log("Collision avec Trap");
-            Respawn();
-        }
-        if (collision.gameObject.CompareTag("Finish"))
-        {
-            popupManager.ShowPopup("Bravo, tu as marqu� + que le jeu");
-            StopTimer();
-        }
-    }
-
     public void StartGame()
     {
         isTimerRunning = true;
@@ -72,12 +50,6 @@ public class BallAction : MonoBehaviour
         int minutes = Mathf.FloorToInt((elapsedTime % 3600) / 60);
         int seconds = Mathf.FloorToInt(elapsedTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
-    }
-
-    void Respawn()
-    {
-        GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
-        transform.position = spawn.GetComponent<Transform>().position;
     }
 
     public void ClosePause()
